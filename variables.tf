2099d843cf4ea25d1ca29c4020be686234325d3c
荣白 <lennybai@live.cn>
2022-03-30 20:14:27 +0800

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -6,7 +6,7 @@ variable "create_certificate" {
 
 variable "certificate_name" {
   type        = string
-  description = "The Name of the Certificate. This name without suffix can have a string of 1 to 63 characters, must contain only alphanumeric characters or " - ", and must not begin or end with " - ", and " - " must not in the 3th and 4th character positions at the same time."
+  description = "The Name of the Certificate. This name without suffix can have a string of 1 to 63 characters, must contain only alphanumeric characters or ' - ', and must not begin or end with ' - ', and ' - ' must not in the 3th and 4th character positions at the same time."
   default     = null
 }
 
