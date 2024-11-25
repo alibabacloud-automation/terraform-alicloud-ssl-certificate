691512e4f347d76d082e17492bc4bf0f3f0a5c3f
shanye <shanye.xzq@alibaba-inc.com>
2024-11-25 16:54:08 +0800

    examples: improve the examples

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
 output "certificate_name" {
   description = "The Name of the Certificate. This name without suffix can have a string of 1 to 63 characters, must contain only alphanumeric characters or '-', and must not begin or end with '-', and '-' must not in the 3th and 4th character positions at the same time."
-  value       = concat(alicloud_ssl_certificates_service_certificate.certificate.*.certificate_name, [""])[0]
+  value       = concat(alicloud_ssl_certificates_service_certificate.certificate[*].certificate_name, [""])[0]
 }
